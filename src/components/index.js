cff8b387e7f4e3866f28b378842464695a1d15dd
faizan3165 <faizanmehmood3165@gmail.com>
2022-10-14 21:56:48 +0500

    temp fix for bg issue

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Navbar from './Navbar/Navbar';
 import Home from './home/Home';
 import Services from './services/Services';
+import Intro from './Intro/Intro';
 
-export { Navbar, Home, Services };
+export { Navbar, Home, Services, Intro };
