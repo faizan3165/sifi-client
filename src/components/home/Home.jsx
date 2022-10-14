717bd885513505e5e58ae41e0479994af0eb5022
Faizan Mehmood <faizanmehmood3165@gmail.com>
2022-10-14 21:49:05 +0500

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -8,9 +8,10 @@ const Home = () => {
 	return (
 		<main className="home_main_container">
 			<br />
-		<div className="home_main_container">
-    main
-			<Intro />
+			<div className="home_main_container">
+
+				<Intro />
+			</div>
 		</main>
 	);
 };
