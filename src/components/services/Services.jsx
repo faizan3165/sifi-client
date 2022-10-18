c9c27bd205fd4d50ff46cdc685027c40de9b4705
=Afaq Khan <=mafaq6131@gmail.com>
2022-10-18 15:53:05 +0500

    services modification

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,71 +1,115 @@
-import React, { Fragment } from 'react';
-import { Card, Button } from 'react-bootstrap';
-
+import React, {Fragment} from 'react';
+import {Card, Button, Container, Row, Col} from 'react-bootstrap';
 import card_logo from '../../assets/images/card_logo.png';
 
 import './services.css';
 
 const dummyData = [
-	{
-		id: 1,
-		img: card_logo,
-		title: 'Graphic Designing',
-		text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat congue ornare lacinia'
-	},
+  {
+    id: 1,
+    img: card_logo,
+    title: 'Graphic Designing',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat congue ornare lacinia',
+  },
 
-	{
-		id: 2,
-		img: card_logo,
-		title: 'Graphic Designing',
-		text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat congue ornare lacinia'
-	},
+  {
+    id: 2,
+    img: card_logo,
+    title: 'Graphic Designing',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat congue ornare lacinia',
+  },
 
-	{
-		id: 3,
-		img: card_logo,
-		title: 'Graphic Designing',
-		text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat congue ornare lacinia'
-	},
+  {
+    id: 3,
+    img: card_logo,
+    title: 'Graphic Designing',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat congue ornare lacinia',
+  },
 
-	{
-		id: 4,
-		img: card_logo,
-		title: 'Graphic Designing',
-		text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat congue ornare lacinia'
-	}
+  {
+    id: 4,
+    img: card_logo,
+    title: 'Graphic Designing',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat congue ornare lacinia',
+  },
+  {
+    id: 4,
+    img: card_logo,
+    title: 'Graphic Designing',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat congue ornare lacinia',
+  },
+  {
+    id: 4,
+    img: card_logo,
+    title: 'Graphic Designing',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat congue ornare lacinia',
+  }
+  ,
+  {
+    id: 4,
+    img: card_logo,
+    title: 'Graphic Designing',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat congue ornare lacinia',
+  },
+  ,
+  {
+    id: 4,
+    img: card_logo,
+    title: 'Graphic Designing',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat congue ornare lacinia',
+  },
+
 ];
 
 const Services = () => {
-	return (
-		<Fragment>
-			<div className="text-center my-5">
-				<h1 className="display-6 fw-bold">Our Services</h1>
-				<p className="text-primary display-2 text-wrap w-50 mt-3 fw-semibold services_para">
-					We Provide All Kinds Of Business Solutions
-				</p>
-			</div>
+  return (
+    <Fragment>
+		<div className='services_container'>
+      <div className="text-center my-5">
+        <h1 className="display-6 fw-bold">Our Services</h1>
+        <p className="text-primary display-2 text-wrap w-60 mt-3 fw-semibold services_para">
+          We Provide All Kinds Of Business Solutions
+        </p>
+      </div>
 
-			<div className="col-md-12 col-sm-6 my_cols">
-				<div className="d-flex flex-row justify-content-around">
-					{dummyData.map(({ id, img, title, text }) => (
-						<Card key={id} className="rounded-4 shadow-lg services_card p-2">
-							<Card.Img variant="top" src={img} class="my_card_logo" />
+      {/* <div className="col-md-12 col-sm-6 my_cols"> */}
+        {/* <div className="d-flex justify-content-around"> */}
+          <Container className="cards_container">
 
-							<Card.Body>
-								<Card.Title>{title}</Card.Title>
+            <Row >
+			 
+              {dummyData.map (({id, img, title, text}) => (
+				   <Col xs={{ span: 6 }} md={{ span: 1, offset: 2, }}>
+                <Card
+                  key={id}
+                  className="rounded-4 shadow-lg services_card p-2"
+                >
+                  <Card.Img variant="top" src={img} class="my_card_logo" />
 
-								<Card.Text>{text}</Card.Text>
+                  <Card.Body>
+                    <Card.Title>{title}</Card.Title>
 
-								<Button variant="light" bg="dark" className='rounded-4 p-2 fw-semibold'>
-									Read More
-								</Button>
-							</Card.Body>
-						</Card>
-					))}
-				</div>
-			</div>
-		</Fragment>
-	);
+                    <Card.Text>{text}</Card.Text>
+
+                    <Button
+                      variant="light"
+                      bg="dark"
+                      className="rounded-4 p-2 fw-semibold"
+                    >
+                      Read More
+                    </Button>
+                  </Card.Body>
+                </Card>
+				</Col>
+              ))}
+			
+            </Row>
+          </Container>
+
+        {/* </div> */}
+      </div>
+    </Fragment>
+  );
 };
 
 export default Services;
