b67ec0ed5f4accba1713bccf31d27ae5cfac3066
=Afaq Khan <=mafaq6131@gmail.com>
2022-10-26 18:39:29 +0500

    cards added in service section

--- src/components/services/CardsData.jsx
+++ src/components/services/CardsData.jsx
@@ -5,6 +5,12 @@ import card_logo3 from '../../assets/images/ui & ux vector.webp';
 import card_logo4 from '../../assets/images/app dev vector.webp';
 import card_logo5 from '../../assets/images/ios_app_developer.svg';
 import card_logo6 from '../../assets/images/Wordpress.png';
+import card_logo7 from '../../assets/images/ERp.webp';
+import card_logo8 from '../../assets/images/saas.svg';
+import card_logo9 from '../../assets/images/remote-base.webp';
+import card_logo10 from '../../assets/images/crm.webp';
+import card_logo11 from '../../assets/images/BPO-solutions.png';
+import card_logo12 from '../../assets/images/advertising.webp';
 
 
 export const dummyData = [
@@ -47,4 +53,41 @@ export const dummyData = [
     title: 'WordPress Development',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat congue ornare lacinia',
   },
+  {
+    id: 6,
+    img: card_logo7,
+    title: 'Enterprise solutions',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat congue ornare lacinia',
+  },
+  {
+    id: 6,
+    img: card_logo8,
+    title: 'Saas deployment',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat congue ornare lacinia',
+  },
+  {
+    id: 6,
+    img: card_logo9,
+    title: 'Remote deployment',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat congue ornare lacinia',
+  },
+  {
+    id: 6,
+    img: card_logo10,
+    title: 'BPO solutions',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat congue ornare lacinia',
+  },
+  {
+    id: 6,
+    img: card_logo11,
+    title: 'Industrial Solutions (CRM)',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat congue ornare lacinia',
+  },
+  {
+    id: 6,
+    img: card_logo12,
+    title: 'advertising solutions',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat congue ornare lacinia',
+  },
+  
 ];
